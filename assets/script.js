a47047c1bd05238240bdbc2582cdf32fbaf20ed0
eljayman <lelandoj@gmail.com>
2022-10-19 22:18:46 -0600

    created the working hours fields with buttons and inputs

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,31 @@
 function currentTime() {
   var dayJsObject = dayjs();
-  document.getElementById("currentDay").innerText =
-    dayJsObject.format("M/D/YYYY h:mm:ss A");
-  console.log("tick");
+  const currentDay = document.getElementById("currentDay");
+  currentDay.innerText = dayJsObject.format("M/D/YYYY h:mm:ss A");
 }
-setTimeout(currentTime, 0);
+$(document).ready(currentTime());
 setInterval(currentTime, 1000);
 
+var workingHours = [
+  "9 AM",
+  "10 AM",
+  "11 AM",
+  "12 AM",
+  "1 PM",
+  "3 PM",
+  "4 PM",
+  "5 PM",
+];
+var $rowEl = $("div");
 
+function createHour() {
+  for (var i = 0; i < workingHours.length; i++) {
+    $rowEl.append(
+      `<div class='row'><time>${workingHours[i]}</time><textarea></textarea><button>Save</button></div>`
+    );
+    const $rowHourEl = $("time").addClass("hour");
+    const $userFieldEl = $("textarea").addClass("time-block");
+    const $saveBtn = $("button").addClass("saveBtn");
+  }
+}
+setTimeout(createHour, 0);
